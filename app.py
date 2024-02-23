1fc2a1d872f6020e4844a49d582ed2d311ba18cd
Christu Rufina M <111833322+ChristuRufina@users.noreply.github.com>
2024-02-23 14:12:53 +0530

    Add files via upload
    Create Your own environment to work with python.
    Use requirements.txt to install all the prerequisties.

---
+++ app.py
@@ -0,0 +1,50 @@
+from dotenv import load_dotenv
+load_dotenv()
+import streamlit as st
+import os
+import google.generativeai as genai
+from PIL import Image
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+def get_gemini_repsonse(input,image,prompt):
+    model=genai.GenerativeModel('gemini-pro-vision')
+    response=model.generate_content([input,image[0],prompt])
+    return response.text
+
+def input_image_setup(uploaded_file):
+    if uploaded_file is not None:
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+
+st.set_page_config(page_title="Image Analysis")
+
+st.header("Image Analysis Using Gemini Pro Vision")
+input=st.text_input("Input Prompt: ",key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+
+submit=st.button("Submit")
+
+input_prompt="""
+Is the person happy or not in the image that i have given to you?
+
+"""
+
+if submit:
+    image_data=input_image_setup(uploaded_file)
+    response=get_gemini_repsonse(input_prompt,image_data,input)
+    st.subheader("The Response is")
+    st.write(response)
+
